8ad1aa0ca33a84440f9b0a978fd27ec2902211f1
paulacarvani <3312@holbertonschool.com>
2021-08-24 17:20:09 -0700

    Duplicates a string in a new variable

---
+++ _strdup.c
@@ -0,0 +1,23 @@
+#include "shell.h"
+
+/**
+ * _strdup - Duplicates a string in a new variable
+ *@s1: String to duplicates
+ *Return: A new variable with the duplicate string
+ */
+char *_strdup(char *s1)
+{
+int i = 0;
+char *dupl = malloc(sizeof(char) * (_strlen(s1) + 1));
+if (dupl == NULL)
+{
+return (NULL);
+}
+while (s1[i] != '\0')
+{
+dupl[i] = s1[i];
+i++;
+}
+dupl[i] = '\0';
+return (dupl);
+}
