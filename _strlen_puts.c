9953b29582052fd1c1283e64b3e0d51d42517adf
afecego <afecego@gmail.com>
2021-08-25 12:25:37 -0500

    Changue call void in the function _puts

---
+++ _strlen_puts.c
@@ -0,0 +1,26 @@
+#include "shell.h"
+
+/**
+ * _strlen - Counts the lenght of a string
+ *@str: String that the function counts
+ *Return: Number of characters of the string
+ */
+unsigned int _strlen(char *str)
+{
+int i = 0;
+while (str[i])
+i++;
+return (i);
+}
+
+/**
+ * _puts - Display a string
+ *@str: String to display
+ *Return: Nothing
+ */
+void _puts(char *str)
+{
+int num = 0;
+num = _strlen(str);
+write(STDOUT_FILENO, str, num);
+}
